8a49cfd94a5f236699a9a122d24bd0c483e062ae
Ricardo SIGNES <rjbs@cpan.org>
2009-10-27 15:36:11 +0000

    Import of RJBS/Pod-Elemental-Transformer-WikiDoc-0.093000 from CPAN.
    gitpan-cpan-distribution: Pod-Elemental-Transformer-WikiDoc
    gitpan-cpan-version:      0.093000
    gitpan-cpan-path:         RJBS/Pod-Elemental-Transformer-WikiDoc-0.093000.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Pod/Weaver/Plugin/WikiDoc.pm
@@ -0,0 +1,52 @@
+package Pod::Weaver::Plugin::WikiDoc;
+our $VERSION = '0.093000';
+
+
+use Moose;
+with 'Pod::Weaver::Role::Dialect';
+# ABSTRACT: allow wikidoc-format regions to be translated during dialect phase
+
+
+use namespace::autoclean;
+
+use Pod::Elemental::Transformer::WikiDoc;
+
+sub translate_dialect {
+  my ($self, $pod_document) = @_;
+
+  Pod::Elemental::Transformer::WikiDoc->new->transform_node($pod_document);
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Pod::Weaver::Plugin::WikiDoc - allow wikidoc-format regions to be translated during dialect phase
+
+=head1 VERSION
+
+version 0.093000
+
+=head1 OVERVIEW
+
+This plugin is an exceedingly thin wrapper around
+L<Pod::Elemental::Transformer::WikiDoc>.  When you load this plugin, then
+C<=begin> and C<=for> regions with the C<wikidoc> format will be translated to
+standard Pod5 before further weaving continues.
+
+=head1 AUTHOR
+
+  Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
